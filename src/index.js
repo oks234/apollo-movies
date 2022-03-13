55269c473e4b0e74aeb330c393936982c7dbf011
dean-oks <oks234@deancorporation.com>
2022-03-13 14:52:14 +0900

    #1.0 Apollo Client

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
+import { ApolloProvider } from "@apollo/client";
 import React from "react";
 import ReactDOM from "react-dom";
+import client from "./apollo";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
