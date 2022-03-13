666ff18a4e57b4880e1b1614bddba5823ed328ff
dean-oks <oks234@deancorporation.com>
2022-03-13 14:40:59 +0900

    #0.4 Router and Styles

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,18 @@
+import React from "react";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+
+import Detail from "../routes/Detail";
+import Home from "../routes/Home";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={Home} />
+        <Route path="/:id" element={Detail} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
